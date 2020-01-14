16f562e35674adcb8eb3b56486ffec0b8a24c209
Erasmo Ramirez <erasmoramirez405@gmail.com>
2020-01-14 11:22:36 -0400

    App finished, ready for deployment

--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ msg: "user already exist" });
+        res.status(400).json({ msg: "User already exist" });
       }
 
       user = new User({
