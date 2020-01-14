0e1efbf6907e754ebb54c11f2098ca08ee02fe09
Erasmo Ramirez <erasmoramirez405@gmail.com>
2020-01-14 12:18:00 -0400

    go back it does not work

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
-    res
-      .setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict")
-      .sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
